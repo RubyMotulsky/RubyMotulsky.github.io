836988dc687226ad411a87b2bbb8d98016f3a9f2
RubyMotulsky <31771834+RubyMotulsky@users.noreply.github.com>
2017-10-20 08:27:01 -0700

    8ball

--- Main.js
+++ Main.js
@@ -6,5 +6,7 @@ function testjs (){
 
 function shake8ball(){
 	document.getElementById("the8").src="images.jpeg";
-	alert("we shall see.");
+	//alert("we shall see.");
+
+	document.getElementById(results).innerHTML="We Shall See";
 }

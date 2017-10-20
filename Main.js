8806c0980f12324ad5c3ff84a8468334d2151b0d
RubyMotulsky <31771834+RubyMotulsky@users.noreply.github.com>
2017-10-20 09:24:15 -0700

    random number

--- Main.js
+++ Main.js
@@ -9,6 +9,10 @@ var list = ["We Shall See",
 			"You wish",
 			"Try Again Later"];
 
+function randomNumber (){
+	return Math.floor (Math.random () * 10);
+}
+
 function testjs (){
 	alert("test");
 }
@@ -16,6 +20,7 @@ function testjs (){
 function shake8ball(){
 	document.getElementById("the8").src="images.jpeg";
 	//alert("we shall see.");
-	document.getElementById("results").innerHTML=list[2];
-	//alert(list[2]);
+	document.getElementById("results").innerHTML=list[randomNumber()];
+
+	//alert(randomNumber());
 }

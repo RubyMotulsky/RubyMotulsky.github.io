de1bc2e0a10b483ec28f79e6d16ab9e463f63ddc
RubyMotulsky <31771834+RubyMotulsky@users.noreply.github.com>
2017-12-01 08:55:09 -0800

    clear board

--- Main.js
+++ Main.js
@@ -30,8 +30,18 @@ function shake8ball(){
 function turn(location){
 	document.getElementById(location).innerHTML = "x";
 }
-
-
+//clears x and o
+function clearAll(){
+	document.getElementById("r1c1").innerHTML = "";
+	document.getElementById("r1c2").innerHTML = "";
+	document.getElementById("r1c3").innerHTML = "";
+	document.getElementById("r2c1").innerHTML = "";
+	document.getElementById("r2c2").innerHTML = "";
+	document.getElementById("r2c3").innerHTML = "";
+	document.getElementById("r3c1").innerHTML = "";
+	document.getElementById("r3c2").innerHTML = "";
+	document.getElementById("r3c3").innerHTML = "";
+}
 
 
 

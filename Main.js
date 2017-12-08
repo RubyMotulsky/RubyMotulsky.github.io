89c570282f8011164136df35a2600462f60e9de0
RubyMotulsky <31771834+RubyMotulsky@users.noreply.github.com>
2017-12-08 08:55:39 -0800

    dsfdgh

--- Main.js
+++ Main.js
@@ -56,7 +56,24 @@ function clearAll(){
 	document.getElementById("r3c3").innerHTML = "";
 }
 
+function checkWinner(){
 
+	var r1c1 = document.getElementById("r1c1").innerHTML;
+	var r1c2 = document.getElementById("r1c2").innerHTML;
+	var r1c3 = document.getElementById("r1c3").innerHTML;
+	var r2c1 = document.getElementById("r2c1").innerHTML;
+	var r2c2 = document.getElementById("r2c2").innerHTML;
+	var r2c3 = document.getElementById("r2c3").innerHTML;
+	var r3c1 = document.getElementById("r3c1").innerHTML;
+	var r3c2 = document.getElementById("r3c2").innerHTML;
+	var r3c3 = document.getElementById("r3c3").innerHTML;
+
+	if(r1c1 == r1c2 && r1c1 == r1c3 && r1c1 != "" ||
+	   r2c1 == r2c2 && r2c1 == r2c3 && r2c1 != "" ||) {
+
+		alert("Winner!");
+	}
+}
 
 
 

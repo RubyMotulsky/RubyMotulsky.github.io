f0bade34085d92c49fc007f71fceea47d3e5951d
RubyMotulsky <31771834+RubyMotulsky@users.noreply.github.com>
2018-01-26 09:39:41 -0800

    hgfds

--- Flashcards.js
+++ Flashcards.js
@@ -3,7 +3,10 @@ function addNumbers(){
 	var operand1 = Document.getElementById("operand1").value;
 	var operand2 = Document.getElementById("operand2").value;
 
-	var sum = operand1 + operand2;
+
+	var sum = parseInt(operand1) + parseInt(operand2);
 
 	alert(sum);
+
+
 }

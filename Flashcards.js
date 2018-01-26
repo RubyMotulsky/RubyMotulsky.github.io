51e9165b9a5678046fef3568bec584d40b030d01
RubyMotulsky <31771834+RubyMotulsky@users.noreply.github.com>
2018-01-26 09:24:24 -0800

    hjkgfcd

---
+++ Flashcards.js
@@ -0,0 +1,9 @@
+function addNumbers(){
+
+	var operand1 = Document.getElementById("operand1").value;
+	var operand2 = Document.getElementById("operand2").value;
+
+	var sum = operand1 + operand2;
+
+	alert(sum);
+}
